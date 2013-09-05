bfc7fd3f468fe95f9deadc195f615f5d3363a78d
Algimantas Stancelis <algimantas@impresspages.org>
2013-09-05 17:38:58 +0300

    Example content

--- exampleContent/exampleIpContent.php
+++ exampleContent/exampleIpContent.php
@@ -4,24 +4,181 @@
  * Example ImpressPages widgets content.
  */
 
+$exampleContentPath = BASE_URL . THEME_DIR . THEME . '/exampleContent';
+
 ?>
-
 <div class="ipWidget ipPreviewWidget ipWidget-IpTitle ipLayout-default">
-    <h1 class="ipwTitle">Example H1 Title</h1>
+        <h1 class="ipwTitle">Lorem ipsum</h1>
+    </div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpText ipLayout-default">
+        <p><span>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick <strong>wafting zephyrs</strong> vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex.</span></p>
+        <ul>
+            <li>Mega man</li>
+            <li>Super Mario Bros</li>
+            <li>Street Fighter</li>
+            <li>Pac Man</li>
+        </ul>
+        <p><span>It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.</span></p>
+        <ol>
+            <li><a href="http://www.imdb.com/title/tt0111161/">The Shawshank Redemption</a></li>
+            <li>The Godfather</li>
+            <li>Pulp Fiction</li>
+            <li>The Good, the Bad and the Ugly</li>
+            <li>The Dark Knight</li>
+        </ol></div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpSeparator ipLayout-default">
+    </div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpTitle ipLayout-level2">
+        <h2 class="ipwTitle">Paragraphs</h2>
+    </div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpTextImage ipLayout-default">
+        <img class="ipwImage" src="<?php echo $exampleContentPath ?>/file/_DSC0182_1.jpg" alt="">
+        <div class="ipwText">
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p></div>
+    </div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpImage ipLayout-default">
+        <img src="<?php echo $exampleContentPath ?>/file/A_photo_from_personal_collection_2.jpg" alt="A photo from personal collection" title="A photo from personal collection">
+    </div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpImageGallery ipLayout-default">
+        <ul>
+            <li>
+                <a href="<?php echo $exampleContentPath ?>/file/DSC_0053.jpg" title="DSC_0053.jpg">
+                    <img src="<?php echo $exampleContentPath ?>/file/DSC_0053.png" alt="DSC_0053.jpg">
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo $exampleContentPath ?>/file/_DSC0155.jpg" title="_DSC0155.jpg">
+                    <img src="<?php echo $exampleContentPath ?>/file/_DSC0155_1.jpg" alt="_DSC0155.jpg">
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo $exampleContentPath ?>/file/DSC_0138.jpg" title="DSC_0138.jpg">
+                    <img src="<?php echo $exampleContentPath ?>/file/DSC_0138_1.jpg" alt="DSC_0138.jpg">
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo $exampleContentPath ?>/file/DSC_0233.jpg" title="DSC_0233.jpg">
+                    <img src="<?php echo $exampleContentPath ?>/file/DSC_0233_1.jpg" alt="DSC_0233.jpg">
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo $exampleContentPath ?>/file/DSC_0276.jpg" title="DSC_0276.jpg">
+                    <img src="<?php echo $exampleContentPath ?>/file/DSC_0276_1.jpg" alt="DSC_0276.jpg">
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo $exampleContentPath ?>/file/_DSC0112.jpg" title="_DSC0112.jpg">
+                    <img src="<?php echo $exampleContentPath ?>/file/_DSC0112_1.jpg" alt="_DSC0112.jpg">
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpTitle ipLayout-level3">
+        <h3 class="ipwTitle">Logo gallery</h3>
+    </div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpLogoGallery ipLayout-default">
+        <ul>
+            <li>
+                <img src="<?php echo $exampleContentPath ?>/file/Color.png" alt="Color">
+            </li>
+            <li>
+                <img src="<?php echo $exampleContentPath ?>/file/Color_for_white.png" alt="Color for white">
+            </li>
+            <li>
+                <img src="<?php echo $exampleContentPath ?>/file/Black.png" alt="Black">
+            </li>
+            <li>
+                <img src="<?php echo $exampleContentPath ?>/file/White_for_dark_backgrounds.png" alt="White for dark backgrounds">
+            </li>
+        </ul>
+    </div>
+    <div class="ipWidget ipPreviewWidget ipWidget-IpTable ipLayout-default">
+        <table>
+            <tbody>
+            <tr><th>#</th><th>Top ten most populous countries</th><th>1990</th><th>2008</th><th>2025*</th></tr>
+            <tr>
+                <td>1</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_China" title="Demographics of China">China</a></td>
+                <td>1,141</td>
+                <td>1,333</td>
+                <td>1,458</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_India" title="Demographics of India">India</a></td>
+                <td>849</td>
+                <td>1,140</td>
+                <td>1,398</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_the_United_States" title="Demographics of the United States">United States</a></td>
+                <td>250</td>
+                <td>304</td>
+                <td>352</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_Indonesia" title="Demographics of Indonesia">Indonesia</a></td>
+                <td>178</td>
+                <td>228</td>
+                <td>273</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_Brazil" title="Demographics of Brazil">Brazil</a></td>
+                <td>150</td>
+                <td>192</td>
+                <td>223</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_Pakistan" title="Demographics of Pakistan">Pakistan</a></td>
+                <td>108</td>
+                <td>166</td>
+                <td>226</td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_Bangladesh" title="Demographics of Bangladesh">Bangladesh</a></td>
+                <td>116</td>
+                <td>160</td>
+                <td>198</td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_Nigeria" title="Demographics of Nigeria">Nigeria</a></td>
+                <td>94</td>
+                <td>151</td>
+                <td>208</td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_Russia" title="Demographics of Russia">Russia</a></td>
+                <td>148</td>
+                <td>142</td>
+                <td>137</td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td><a href="http://en.wikipedia.org/wiki/Demographics_of_Japan" title="Demographics of Japan">Japan</a></td>
+                <td>124</td>
+                <td>128</td>
+                <td>126</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><b>World total</b></td>
+                <td>5,265</td>
+                <td>6,688</td>
+                <td>8,004</td>
+            </tr>
+            </tbody>
+        </table>
+        <p>Source - <a href="http://en.wikipedia.org/wiki/World_population" target="_blank">World Population</a></p>
 </div>
-
-<div class="ipWidget ipPreviewWidget ipWidget-IpText ipLayout-default">
-    <p>Example simple text.</p>
-</div>
-
-<div class="ipWidget ipPreviewWidget ipWidget-IpTitle ipLayout-default">
-    <h2 class="ipwTitle">Example H2 Title</h2>
-</div>
-
-<div class="ipWidget ipPreviewWidget ipWidget-IpTitle ipLayout-default">
-    <h3 class="ipwTitle">Example H3 Title</h3>
-</div>
-
 
 <!--
     Forms
